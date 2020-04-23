86d96e654bb611a62cc172994a2c2a64e252575a
Mateo Morera <moreramateo@gmail.com>
2020-04-23 16:56:34 -0500

    Agregue los proyectos
    Agregue parte de los proyecots

---
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class HolaMundo {
+
+    public static void main(String args[]) {
+   
+        Scanner scanner = new Scanner(System.in); 
+    
+
+    }
+
+}
